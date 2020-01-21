5a017472a6779bd004ba43988f32f1044feb881a
Paula <gomespaula123@gmail.com>
2020-01-21 15:14:56 +0100

    commit board_visual window working

--- tictactoe/board_visual.py
+++ tictactoe/board_visual.py
@@ -3,15 +3,18 @@ import pygame
 
 
 class BoardVisual:
-    def __init__(self):
-        self.board = [" "] * 9
-        # self.row_length = int(math.sqrt(len(self.board)))
 
-    def create_board(self):
+    def __init__(self, game):
+        print("this exists")
+        # self.row_length = int(math.sqrt(len(self.board)))
+        # self.create_board()
+        self.game = game
+
+    def create_window(self):
         print("it has been called for once")
         background_colour = (255, 255, 255)
         screen = pygame.display.set_mode((700, 700))
-        self.screen.fill(background_colour)
+        screen.fill(background_colour)
         pygame.display.flip()
         running = True
         while running:
