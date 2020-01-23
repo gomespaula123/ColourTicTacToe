20adb21248c8ec8695bc9251cf4a06f518db2101
Paula <gomespaula123@gmail.com>
2020-01-23 03:40:26 +0100

    commit full repository 20

--- tictactoe/ai_player.py
+++ tictactoe/ai_player.py
@@ -4,7 +4,10 @@ class AIPlayer:
 
     def __init__(self, game):
         # print("what do I put here")
+        self.mark = "X"
         self.game = game
+        self.memo = dict()
+        self.counter = 0
 
     def set_board(self, boardvisual):
         self.boardvisual = boardvisual
@@ -18,9 +21,11 @@ class AIPlayer:
         # for next_move in current_board.get_possible_moves():
             # next_score =
         score_move_pairs = []
-
-        for next_move in self.get_possible_moves.possible_moves:
+        print("got till line 23")
+        for next_move in self.get_possible_moves():
+            print("got till line 25)")
             next_score = self.min_max(self.boardvisual.positions_status, next_move, self.mark)
+            print("it did do line 27")
             score_move_pairs.append((next_score, next_move))
 
             # if there is no score/move pair, return 0
@@ -32,6 +37,7 @@ class AIPlayer:
                 # compute the max score/move
                 highest_score, best_move = max(score_move_pairs)
                 # return the move
+                print("best move", best_move)
                 return best_move
 
 
@@ -58,6 +64,8 @@ class AIPlayer:
     def min_max(self, current_board, move, mark):
         # depth = 8
         # the  next  board  is a (deep) copy of the  current  board
+        current_board = self.boardvisual.positions_status
+        print("did a thing, line 68")
         next_board = current_board.deep_copy()
 
         # place  the  move  for  the  mark on the  next  board --> put best move there
@@ -115,17 +123,17 @@ class AIPlayer:
         # compute  the  maximum  score  of all  possible  moves
         score = []
         for possible_move in next_board.get_possible_moves():
-            depth -= 1
+            '''depth -= 1
             if depth <= -1:
                 score.append(-1 * (self.get_heuristic_value(next_board, mark)))  # add the scores of the possible moves of your opponent
                 self.counter += 1
-                return max(score)
+                return max(score)'''
             score_value = self.min_max(next_board, possible_move, self.other_mark(mark))
             score.append(score_value)
 
         self.memo[(tuple(next_board.board))] = max(score)
         self.counter += 1
-        print("maxmin counter", self.get_counter())
+        # print("maxmin counter", self.get_counter())
         # print("depth maxmin", depth)
 
         # return the maximum score
@@ -142,9 +150,9 @@ class AIPlayer:
         for i in range(0, 9):
             if self.boardvisual.positions_status[i] == 0:
                 possible_moves.append(self.boardvisual.positions_status[i])
-                print("it got added")
-                print(i)
+                # print("it got added")
+               #  print(i)
         # for self.boardvisual.positions_status in possible_moves:
              #print i
-
-        #print(possible_moves)
+        print(possible_moves)
+        return possible_moves
